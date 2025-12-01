f3e5906d7eff8e0139e870428960d63c12839892
rotemd0201 <rotemd0201@gmail.com>
2025-12-01 21:52:20 +0200

    fear (SafeAndSound) - finushed part 2, debbuging

--- DebugWorkshop/SafeAndSound/part2.cpp
+++ DebugWorkshop/SafeAndSound/part2.cpp
@@ -5,12 +5,14 @@
 char* safe_string_copy(char* dest, unsigned int destsize, char* src)
 {
 	unsigned int srcsize = (unsigned int)strlen(src);
-	if (srcsize >= destsize)
+
+	if (srcsize > destsize)
 		throw std::overflow_error("possible buffer overflow");
 
 	char* ret = dest;
-	for (unsigned int i = 0; i < srcsize * sizeof(*src); i++)
+	for (unsigned int i = 0; i <= srcsize * sizeof(*src); i++)
 		*dest++ = *src++;
+
 	return ret;
 }
 
