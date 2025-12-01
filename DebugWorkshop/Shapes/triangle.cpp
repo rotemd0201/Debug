afee458e3c0bc399cc251132b564a8e5078983fc
rotemd0201 <rotemd0201@gmail.com>
2025-12-01 20:38:11 +0200

    Debugging work shop

---
+++ DebugWorkshop/Shapes/triangle.cpp
@@ -0,0 +1,14 @@
+#include "triangle.h"
+#include <exception>
+
+
+Triangle::Triangle(float base, float height) : _base(base), _height(height)
+{
+}
+
+float Triangle::get_area() const
+{
+	return float(0.5 * _base * _height);
+}
+
+
